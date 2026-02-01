fd4ac21c66d6fb73c20c8f753824af9d0868d435
parchii <wainggan@gmail.com>
2026-02-01 06:03:25 -0800

    db define

---
+++ src/db/db.types.ts
@@ -0,0 +1,16 @@
+import { Miss } from "../common.ts";
+
+export type User = {
+	id: number;
+	username: string;
+	password: string;
+}
+
+type internal = 'internal';
+type not_found = 'not_found';
+
+export interface DB {
+	users_new(username: string, password: string): Promise<null | Miss<internal>>;
+	users_get(username: string): Promise<User | Miss<internal | not_found>>;
+}
+
