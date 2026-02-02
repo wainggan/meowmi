15b68755f9f63676a5522fa315884cfeafc47006
parchii <parchii@waifu.club>
2026-02-01 17:58:51 -0800

    hide lock

--- src/route/template.tsx
+++ src/route/template.tsx
@@ -12,6 +12,7 @@ export const Base = (input: { title: string }, children: unknown[]) => {
 			<head>
 				<meta charset="utf-8" />
 				<meta name="viewport" content="width=device-width,initial-scale=1.0" />
+				<link rel="stylesheet" href="/static/style.css"/>
 				<title>{ input.title }</title>
 			</head>
 			<body>
