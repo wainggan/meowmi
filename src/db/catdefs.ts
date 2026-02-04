6d22ec7aad5d5d5ad284f0a4083732ae25afcbac
parchii <wainggan@gmail.com>
2026-02-03 21:21:43 -0800

    defs

--- src/db/catdefs.ts
+++ src/db/catdefs.ts
@@ -1,18 +1,48 @@
 
+type CatDefRarities =
+	| 'common'
+	| 'uncommon'
+	| 'rare';
+
 type CatDef = {
 	readonly name: string;
+	readonly rarity: CatDefRarities;
 };
 
-const map = new Map<number, CatDef>();
+type CatDefLoot = {
+	readonly rarities: {
+		readonly [key in CatDefRarities]: number;
+	};
+	readonly specific: {
+		readonly [key in keyof typeof map]?: number;
+	};
+};
 
-map.set(0, {
-	name: 'basic',
-});
+const map = {
+	1: {
+		name: 'basic',
+		rarity: 'common',
+	},
+} as const satisfies Record<number, CatDef>;
 
-const keys = map.keys().toArray();
+const keys = Object.keys(map).map(x => Number(x)) as unknown as readonly (keyof typeof map)[];
+
+const loot = {
+	base: {
+		rarities: {
+			common: 8,
+			uncommon: 4,
+			rare: 2,
+		},
+		specific: {},
+	},
+} as const satisfies {
+	readonly [key: string]: CatDefLoot;
+};
 
 export default {
 	map,
 	keys,
+	loot,
 };
 
